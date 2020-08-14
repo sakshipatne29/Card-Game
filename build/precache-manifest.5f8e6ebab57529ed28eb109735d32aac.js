4d133085630dfa9e46487cc6c78bddb61c2292a4
sakshipatne29 <sakshipatne206@gmail.com>
2020-08-14 23:49:17 +0530

    MyProject

---
+++ build/precache-manifest.5f8e6ebab57529ed28eb109735d32aac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104794e0fb11bae4c04f9646f54f2424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7000569d86c1cb2df42",
+    "url": "/static/js/2.fae634e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fae634e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8180959f0445b67d78dc",
+    "url": "/static/js/main.176e17a7.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
